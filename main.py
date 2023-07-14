7c56b01531b46d136d9e793442b277cc8a1b1e75
maksim.prybylski <makspr94@gmail.com>
2023-07-14 09:11:15 +0300

    day 15 initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0,
+}
+
+# TODO: 1. print report
+
+def report(resources):
+    for key, value in resources.items():
+        print (key, ": ", value, sep='')
+
+
+
+# TODO: 2. Check resource sufficient
+
+
+#: TODO 3. process coins
+
+
+# TODO 4. Check transaction
+
+
+# TODO 5. make coffee
+
+
+report (resources)
